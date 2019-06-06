2f4e8fa89b93582d573ad456e8ce016c287fcda9
irwanphan <irwanphan@gmail.com>
2019-06-06 21:28:26 +0700

    add ion-header and ion-content

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,27 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="ion-page">
+    <ion-header>
+      <ion-toolbar>
+        <ion-title>
+          ZipInfo
+        </ion-title>
+      </ion-toolbar>
+    </ion-header>
+
+    <ion-content class="ion-padding">
+      My App
+    </ion-content>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+
 
 export default {
   name: 'home',
   components: {
-    HelloWorld
+
   }
 }
 </script>
